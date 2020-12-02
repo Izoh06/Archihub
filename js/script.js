aeba6ad65d9ef6e3609b5539d82eab0993f87385
Izoh06 <isaacmutugi06@gmail.com>
2020-12-02 16:11:56 +0300

    First commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ $(function (){
         time: 2000
     });
 });
+
+
+
+const nav = document.querySelector('.navbar');
+
+window.onscroll = function() {
+	var top = window.scrollY;
+
+	if(top >= 50){
+		nav.classList.add('active')
+	}
+	else{
+		nav.classList.remove('active');
+	}
+}
