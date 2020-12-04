151ec28ba879ecbb5ac5bf0bb5cac57c791ce78c
Izoh06 <isaacmutugi06@gmail.com>
2020-12-04 09:56:26 +0300

    2nd commit

---
+++ header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="plan,architecture,bedroom,design,archihub,interior,architect,construction,archi-hub,archihub studio,archi-hub studio,plan request,plan-request,landscaping,house,approval,structural,BORAX,county,submission,approval">
+	<title>Archihub Studio</title>
+	
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	
+	<!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="css/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl-carousel/owl.theme.default.min.css">
+    
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     
+      
+  <!--Magnific popup-->
+  <link rel="stylesheet" href="css/magnific-popup/magnific-popup.css">
+  
+  <!--CUSTOM CSS-->
+  <link rel="stylesheet" href="./swiper-bundle.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+
+  <style>
+    body {
+      background: #fff;
+      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color: #000;
+      margin: 0;
+      padding: 0;
+    }
+
+    .swiper-container {
+      width: 100%;
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+
+    .swiper-slide {
+      background-position: center;
+      background-size: cover;
+      width: 300px;
+      height: 300px;
+
+    }
+
+
+  </style>
+
+
+
+</head>
+<body class="bg-light">
+
+<!--Header-->
+
+
+
+<div class="container text-dark" id="home">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 shadow" >
+      <a class="navbar-brand ml-5" href="#index.html">ARCHIHUB</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav navBar_resp mr-auto my-1" >
+              <li class="nav-item"><a href="index.html">HOME</a></li>
+              <li class="nav-item"><a href="Planrequest.html">PLAN REQUEST</a></li>
+              <li class="nav-item"><a href="Project.html">PROJECTS</a></li>
+              <li class="nav-item"><a href="services.html">SERVICES</a></li>
+              <li class="nav-item"><a href="Aboutus.html">ABOUT</a></li>
+              <li class="nav-item"><a href="staff.html">STAFF</a></li>
+              <li class="nav-item"><a href="Contactus.html">CONTACT US</a></li>
+          </ul>
+        </div>
+    
+    </nav>
+    </div>
--- index.php
+++ index.php
@@ -1,88 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="keywords" content="plan,architecture,bedroom,design,archihub,interior,architect,construction,archi-hub,archihub studio,archi-hub studio,plan request,plan-request,landscaping,house,approval,structural,BORAX,county,submission,approval">
-	<title>Archihub Studio</title>
-	
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	
-	<!-- Owl Carousel CSS -->
-    <link rel="stylesheet" href="css/owl-carousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl-carousel/owl.theme.default.min.css">
-    
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-     
-      
-  <!--Magnific popup-->
-  <link rel="stylesheet" href="css/magnific-popup/magnific-popup.css">
-  
-  <!--CUSTOM CSS-->
-  <link rel="stylesheet" href="./swiper-bundle.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/responsive.css">
-
-
-  <style>
-    body {
-      background: #fff;
-      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-      font-size: 14px;
-      color: #000;
-      margin: 0;
-      padding: 0;
-    }
-
-    .swiper-container {
-      width: 100%;
-      padding-top: 50px;
-      padding-bottom: 50px;
-    }
-
-    .swiper-slide {
-      background-position: center;
-      background-size: cover;
-      width: 300px;
-      height: 300px;
-
-    }
-
-
-  </style>
-
-
-
-</head>
-<body class="bg-light">
-
-<!--Header-->
-
-
-
-<div class="container text-dark" id="home">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 shadow" >
-      <a class="navbar-brand ml-5" href="#index.html">ARCHIHUB</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav navBar_resp mr-auto my-1" >
-              <li class="nav-item"><a href="index.html">HOME</a></li>
-              <li class="nav-item"><a href="Planrequest.html">PLAN REQUEST</a></li>
-              <li class="nav-item"><a href="Project.html">PROJECTS</a></li>
-              <li class="nav-item"><a href="services.html">SERVICES</a></li>
-              <li class="nav-item"><a href="Aboutus.html">ABOUT</a></li>
-              <li class="nav-item"><a href="staff.html">STAFF</a></li>
-              <li class="nav-item"><a href="Contactus.html">CONTACT US</a></li>
-          </ul>
-        </div>
-    
-    </nav>
-    </div>
-    <!--End of nav-->
-    
-
+<?php
+     include('header.php');
+?>
 
 <section class="tophouse">
   <div class="container-fluid">
@@ -340,33 +258,7 @@
 
 <!--FOOTER BEGINS-->
 
+<?php
+     include('footer.php');
+?>
 
-
-<!--FOOTER ENDS-->
-
-<!--JQUERY-->
-    <script src="js/jquery.js"></script>
-    
-   <!-- BOOTSTRAP JS-->
-  
-    
-    <!-- Owl Carousel JS -->
-    <script src="js/owl-carousel/owl.carousel.min.js"></script>
-    
-     <!--MAGNIFIC POPUP-->
-   <script src="js/jquery.magnific-popup.min.js"></script>
-   
-     <!--COUNTER-->
-    <script src="js/counter/jquery.counterup.min.js"></script>
-   
-     <!--WAYPOINTS-->
-    <script src="js/waypoints/jquery.waypoints.min.js"></script>
-
-    <script src="bootstrap/js/bootstrap.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-
-   <!--CUSTOM JS-->
-    <script src="js/script.js"></script>
-
-</body>
-</html>
