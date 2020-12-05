bb7b19664b5c58f810d772f233e9318661ff5821
Izoh06 <isaacmutugi06@gmail.com>
2020-12-05 10:44:07 +0300

    php

---
+++ services.php
@@ -0,0 +1,72 @@
+<?php
+include('header.php')
+?>
+</header>
+ 
+<!--HEADER ENDS-->
+
+<!--SERVICES CONTENT BEGINS-->
+
+<div id="services">
+<div class="content-box-md">
+<div class="container">
+<div class="row">
+  <div class="col-md-3">
+   <div class="service">
+   <h5>PLANNING SERVICES:</h5>
+   <hr>
+   <li>Regional land use, market surveys and site selection</li>
+   <li>Site evaluation and feasibility</li>
+   <li>Land negotiations and acquisition</li>
+   <li>Developing master plans.</li>
+   <li>Submission for approvals of master plans to local authorities</li>
+   <li>Urban and Regional Planning</li>
+   <li>Urban Design</li>
+   </div>
+  </div>
+
+  <div class="col-md-3">
+      <div class="service">
+      <h5>DESIGN AND CONSTRUCTION SERVICES:</h5>
+      <hr>
+      <li>Programming</li>
+      <li>Scheme design</li>
+      <li>Final Design</li>
+      <li>Construction Drawings</li>
+      <li>Architectural Coordination during documentation and implementation</li>
+      <li>Construction project management</li>
+      </div>
+  </div>
+
+    <div class="col-md-3">
+        <div class="service">
+        <h5>ADVICES ON MATERIALS AND CONSTRUCTION TECHNOLOGIES</h5>
+        <hr>
+        <li>Use of locally available materials</li>
+        <li>Construction Techniques (modern and vernacular)</li>
+        <li>General Materials Technologies(modern and traditional)</li>
+        </div>
+    </div>
+
+  <div class="col-md-3">
+   <div class="service">
+   <h5>SUPPLEMENTARY SERVICES:</h5>
+   <hr>
+   <li>Preparation of promotional and advertisement materials</li>
+   <li>Landscaping</li>
+   <li>Interior Design</li>
+   </div>
+  </div>
+
+</div>
+</div>
+</div>
+</div>
+
+<!--SERVICES CONTENT ENDS-->
+
+<!--FOOTER BEGINS-->
+
+<?php
+include('footer.php')
+?>
---
+++ staff.php
@@ -0,0 +1,69 @@
+<?php
+include('header.php')
+?>
+</header>
+ 
+<!--HEADER ENDS-->
+
+ <!--TABLE BEGINS-->
+
+<div class="container" style="margin-top: 80px;">
+ <table>
+    <tr>
+      <th>Name</th>
+      <th>Qualification</th>
+      <th>Experience</th>
+    </tr>
+    <tr>
+      <td>James Kirimi</td>
+      <td>B.Architectural studies,<br>B.Architecture UoN,<br>Phd Project Management</td>
+      <td>4 Years</td>
+    </tr>
+    <tr>
+      <td>Hillary Ng'etich</td>
+      <td>B.Architectural studies,<br>B.Architecture</td>
+      <td>4 Years</td>
+    </tr>
+    <tr>
+      <td>Ian Mutugi Kiruja</td>
+      <td>B.Architecture JKUAT</td>
+      <td>4 Years</td>
+    </tr>
+    <tr>
+      <td>Mellise Achieng' Ochieng</td>
+      <td>B.Architecture and<br>B.Architectural studies UoN</td>
+      <td>4 Years</td>
+    </tr>
+    <tr>
+      <td>Jeremiah Mutembei Nyaga</td>
+      <td>B.Architecture<br>TUK</td>
+      <td>2 Years</td>
+    </tr>
+    <tr>
+      <td>Dennis Mwenda Nyaga</td>
+      <td>Dip Architecture,<br>Phd Project Management</td>
+      <td>9 Years</td>
+    </tr>
+  </table>
+</div>
+
+<!--TABLE ENDS-->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src="IMG-20160720-WA0006_1567413590481.jpg"/>
+                <p class="text-center">Frank Mwiti</p>
+            </div>
+            
+             <div class="col-md-6">
+                <img src=""/>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include('footer.php')
+?>
